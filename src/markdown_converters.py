6437d153efd7c51d28a5d0812098158cba51be03
Fahmi Adi Nugraha <fnugraha@gmail.com>
2025-02-06 21:22:16 +0700

    Add block_to_block_type function and associated unit tests.

--- src/markdown_converters.py
+++ src/markdown_converters.py
@@ -1,3 +1,14 @@
+import re
+
+BLOCK_TYPE_PATTERNS = {
+    "heading": r"^#{1,6} .*",
+    "code": r"^`{3}.*`{3}$",
+    "quote": r"^>",
+    "unordered list": r"^(\*|-) ",
+    "ordered list": r"^\d+\. ",
+}
+
+
 def markdown_to_blocks(markdown: str) -> list[str]:
     """Split markdown document into blocks
 
@@ -15,3 +26,73 @@ def markdown_to_blocks(markdown: str) -> list[str]:
         omitted.
     """
     return [block.strip() for block in markdown.split("\n\n") if block]
+
+
+def _check_listlike_block(list_block: list[str], pattern: str) -> list[str]:
+    """Checks to see whether all elements of a list-like block match a pattern
+
+    If any list element doesn't match then the entire block is considered invalid and an
+    empty list is returned. If all elements match then the original list is returned.
+
+    Parameters
+    ----------
+    list_block: list[str]
+        A list of strings where each element corresponds to a list item
+
+    Returns
+    -------
+    list[str]
+        Either the original "list_block" or and empty list if there was an element that
+        did not match the pattern.
+    """
+    for list_el in list_block:
+        if not re.search(pattern, list_el):
+            list_block = []
+            break
+    return list_block
+
+
+def block_to_block_type(block: str) -> str:
+    """Returns the type of the block
+
+    Checks whether "block" is one of the following types:
+
+    - Heading
+    - Code
+    - Quote
+    - Unordered list
+    - Ordered list
+    - Paragraph
+
+    Assumes that "block" was obtained from "markdown_to_blocks" function
+
+    Parameters
+    ----------
+    block: str
+        A string representing a block of text in a markdown document.
+
+    Returns
+    -------
+    str
+        The type of the block.
+    """
+    for block_type, pattern in BLOCK_TYPE_PATTERNS.items():
+        m: list[str] = []
+        match block_type:
+            case "heading":
+                m = re.findall(pattern, block)
+            case "code":
+                m = re.findall(pattern, block, re.S)
+            case "quote" | "unordered list":
+                m = _check_listlike_block(block.split("\n"), pattern)
+            case "ordered list":
+                m = _check_listlike_block(block.split("\n"), pattern)
+                if m:
+                    list_nums = [int(num.split(".")[0]) for num in m]
+                    if sorted(list_nums) != list_nums:
+                        m = []
+            case _:
+                pass
+        if m:
+            return block_type
+    return "paragraph"
--- src/test_markdown_converters.py
+++ src/test_markdown_converters.py
@@ -1,6 +1,6 @@
 import unittest
 
-from markdown_converters import markdown_to_blocks
+from markdown_converters import block_to_block_type, markdown_to_blocks
 
 
 class TestMarkdownToBlocks(unittest.TestCase):
@@ -36,15 +36,15 @@ A *tiny* **paragraph**.
 * Item two
 * Item three
 
-* Item one of a new list
-* Item two"""
+1. Item one of a new list
+2. Item two"""
 
         got = markdown_to_blocks(doc)
         want = [
             "# A document with a lot of spaces",
             "A *tiny* **paragraph**.",
             "* Item one\n* Item two\n* Item three",
-            "* Item one of a new list\n* Item two",
+            "1. Item one of a new list\n2. Item two",
         ]
 
         self.assertEqual(got, want)
@@ -64,5 +64,168 @@ print("This is a code block")
             "Another tiny paragraph.",
             '```python\nprint("This is a code block")\n```',
         ]
+
+        self.assertEqual(got, want)
+
+
+class TestBlockToBlockType(unittest.TestCase):
+    def test_heading_h1(self):
+        block = "# This is an H1 heading"
+        want = "heading"
+        got = block_to_block_type(block)
+
+        self.assertEqual(got, want)
+
+    def test_heading_broken(self):
+        block = "This is a # Broken H1 heading"
+        want = "paragraph"
+        got = block_to_block_type(block)
+
+        self.assertEqual(got, want)
+
+    def test_heading_h3(self):
+        block = "### This is an H3 heading"
+        want = "heading"
+        got = block_to_block_type(block)
+
+        self.assertEqual(got, want)
+
+    def test_heading_h6(self):
+        block = "###### This is an H6 heading"
+        want = "heading"
+        got = block_to_block_type(block)
+
+        self.assertEqual(got, want)
+
+    def test_code(self):
+        block = """```python
+print("This is some Python code")
+
+a = 1
+b = 4
+print(f"a + b = {a+b}")```"""
+        want = "code"
+        got = block_to_block_type(block)
+
+        self.assertEqual(got, want)
+
+    def test_code_single_backtick(self):
+        block = """`print("This is a single line of code")`"""
+        want = "paragraph"
+        got = block_to_block_type(block)
+
+        self.assertEqual(got, want)
+
+    def test_code_unbalanced(self):
+        block = """```python
+print("This is some Python code in an unbalanced code block")
+
+a = 1
+b = 4
+print(f"a + b = {a+b}")"""
+        want = "paragraph"
+        got = block_to_block_type(block)
+
+        self.assertEqual(got, want)
+
+    def test_quote(self):
+        block = """> Some text in a
+> quoted block.
+>There are some sections
+> where there are no spaces
+>    after the quote character
+> or there are several"""
+        want = "quote"
+        got = block_to_block_type(block)
+
+        self.assertEqual(got, want)
+
+    def test_quote_broken(self):
+        block = """> Here is a quoted block where
+there is text > before the quote character.
+> This invalidates the entire block."""
+        want = "paragraph"
+        got = block_to_block_type(block)
+
+        self.assertEqual(got, want)
+
+    def test_unordered_list_hyphen(self):
+        block = """- A proper unordered list
+- using hyphens
+- containing 3 items"""
+        want = "unordered list"
+        got = block_to_block_type(block)
+
+        self.assertEqual(got, want)
+
+    def test_unordered_list_asterisk(self):
+        block = """* A proper unordered list
+* using asterisks
+* containing 3 items"""
+        want = "unordered list"
+        got = block_to_block_type(block)
+
+        self.assertEqual(got, want)
+
+    def test_unordered_list_broken_no_space(self):
+        block = """- An invalid unordered list
+-with no space
+- between the hyphen and text"""
+        want = "paragraph"
+        got = block_to_block_type(block)
+
+        self.assertEqual(got, want)
+
+    def test_unordered_list_broken_character_in_middle(self):
+        block = """- An invalid unordered list
+with the - hyphen
+- in the middle of the text"""
+        want = "paragraph"
+        got = block_to_block_type(block)
+
+        self.assertEqual(got, want)
+
+    def test_ordered_list(self):
+        block = """1. A proper ordered list
+2. with spaces between the
+3. numbers and text and
+4. the numbers themselves in ascending order"""
+        want = "ordered list"
+        got = block_to_block_type(block)
+
+        self.assertEqual(got, want)
+
+    def test_ordered_list_broken_no_space(self):
+        block = """1. An invalid ordered list
+2.with no spaces between
+3. numbers and text"""
+        want = "paragraph"
+        got = block_to_block_type(block)
+
+        self.assertEqual(got, want)
+
+    def test_ordered_list_broken_in_middle(self):
+        block = """1. An invalid ordered list
+with the 2.number
+3. coming in the middle of the text"""
+        want = "paragraph"
+        got = block_to_block_type(block)
+
+        self.assertEqual(got, want)
+
+    def test_ordered_list_unordered(self):
+        block = """1. An invalid ordered list
+4. where the numbers
+2. are out of order
+3. everything else is fine"""
+        want = "paragraph"
+        got = block_to_block_type(block)
+
+        self.assertEqual(got, want)
+
+    def test_paragraph_single(self):
+        block = "This is just some text with nothing extra going on. Should be seen as just a paragraph"
+        want = "paragraph"
+        got = block_to_block_type(block)
 
         self.assertEqual(got, want)
